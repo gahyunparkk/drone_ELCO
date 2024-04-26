26c017c4476d2fafb3f4aa01eb8ab9bfbafa4a60
gahyunparkk <126551872+gahyunparkk@users.noreply.github.com>
2024-04-26 18:27:41 +0900

    Add files via upload

---
+++ drone_project_1.m
@@ -0,0 +1,24 @@
+
+if(month == 4 || month == 6 || month == 9 || month == 11)
+    if(day > 30)
+        month = month + 1;
+        day = day - 30; 
+    end
+elseif(month == 2)
+    if(day > 28) 
+        month = month + 1;
+        day = day - 28;
+    end
+else
+    if(day > 31)
+        month = month + 1;
+        day = day - 31;
+    end
+end
+
+if month > 12
+    year = year + 1;
+    month = month - 12;
+end
+
+fprintf('%d시간 후의 시간: %d년 %d월 %d일 %d시\n',time, year, month, day, hour);
